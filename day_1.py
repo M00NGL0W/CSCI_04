d4a6a14fd0859360f8bd85d82d281e00aced073d
M00NGL0W <nlevang001@student.butte.edu>
2025-02-03 15:15:43 -0800

    Update day_1.py

--- day_1.py
+++ day_1.py
@@ -1,6 +1,10 @@
 print Devil = 2531 
 print HaHa = devil+1 
-print e = 5^5 
-print squid = "octopus+2"
+print e = 5**5
+print squid = "octopus"
 print a = 5 
-print dragon = [1,2,3,4,5]
+print dragon = [2,3,4,5,6,1]
+def sum(x+y):
+  return x+y 
+  
+  l = lambda: 
